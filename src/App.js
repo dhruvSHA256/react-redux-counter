0102d0a7ab95bf8079efdf749f688bf2918d946e
DHruv <dhruv.sha256@gmail.com>
2022-08-26 14:34:42 +0530

    counter with used defined diff

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     // it takes a callback function which in which the whole state is passed
     // it should return the data from state which is in need
     const counter = useSelector((state) => state.counter);
+    const addBy = useSelector((state) => state.addBy);
 
     // useDispatch function help us to dispatch an action and the action is processed by
     // the reducer function, modifying the state of the program
@@ -15,10 +16,23 @@ function App() {
     const handleOnclick = (event) => {
         switch (event.currentTarget.name) {
             case "increment":
-                dispatch({ type: "INC", payload: 1 });
+                dispatch({ type: "INC", payload: addBy });
                 break;
             case "decrement":
-                dispatch({ type: "DEC", payload: 1 });
+                dispatch({ type: "DEC", payload: addBy });
+                break;
+            default:
+                return;
+        }
+    }
+
+    const handleOnchange = (event) => {
+        switch (event.currentTarget.name) {
+
+            case "addBy":
+                const payload = (event.currentTarget.value.length === 0) ? 1
+                    : parseInt(event.currentTarget.value, 10);
+                dispatch({ type: "ADDBY", payload });
                 break;
             default:
                 return;
@@ -29,6 +43,7 @@ function App() {
         <div className="App">
             <h1>Counter</h1>
             <h2> {counter}</h2>
+            <input name="addBy" defaultValue={addBy} onChange={handleOnchange} />
             <button name='increment' onClick={handleOnclick}>Increment</button>
             <button name='decrement' onClick={handleOnclick}>Decrement</button>
         </div>
--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,20 @@ import { createStore } from 'redux';
 // initial state of the program
 const initialState = {
     counter: 0,
+    addBy: 1,
 };
 
 
 // a reducer function takes a state and performs some action (with type and payload) on it and returns a new state
 const reducerFn = (state = initialState, action) => {
-    const { type, payload = 1 } = action;
+    const { type, payload } = action;
     switch (type) {
         case "INC":
             return { ...state, counter: state.counter + payload };
         case "DEC":
             return { ...state, counter: state.counter - payload };
+        case "ADDBY":
+            return { ...state, addBy: payload };
         default:
             return state;
     }
